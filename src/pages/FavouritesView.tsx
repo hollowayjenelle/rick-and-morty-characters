b51b63b1776b1ab7ba98c2ae1212ac2b53fc7f85
jholloway <jholloway@griddynamics.com>
2023-06-27 11:26:13 -0500

    create FavouritesView

---
+++ src/pages/FavouritesView.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FavouritesView : React.FC = () => {
+    return (
+        <div>
+            This is favourites
+        </div>
+    );
+};
+
+export default FavouritesView;
