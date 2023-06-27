a1381474afe35b632200e4f3f0a8423eec23c81a
jholloway <jholloway@griddynamics.com>
2023-06-27 13:01:21 -0500

    change store to persist favourites data

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,16 +1,32 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import { rickAndMortyApi } from "./services/rickAndMortyApi";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import favourites from "./slices/favouritesSlice";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: [rickAndMortyApi.reducerPath],
+};
+
+const rootReducer = combineReducers({
+  [rickAndMortyApi.reducerPath]: rickAndMortyApi.reducer,
+  favourites: favourites,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    [rickAndMortyApi.reducerPath]: rickAndMortyApi.reducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(rickAndMortyApi.middleware),
 });
 
 setupListeners(store.dispatch);
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 
